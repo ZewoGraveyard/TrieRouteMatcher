33a446628914a6050b01af1e867561b243e1b9ec
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-19 17:32:57 -0300

    bump to 0.5.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "TrieRouteMatcher",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/PathParameterMiddleware.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/PathParameterMiddleware.git", majorVersion: 0, minor: 5),
     ]
 )
