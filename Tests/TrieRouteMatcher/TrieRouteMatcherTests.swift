96bff2dc7676a8c90cc4c2df5f1fade657332552
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-25 22:53:45 -0300

    add travis

--- Tests/TrieRouteMatcher/TrieRouteMatcherTests.swift
+++ Tests/TrieRouteMatcher/TrieRouteMatcherTests.swift
@@ -1,37 +1,9 @@
-// TrieRouteMatcherTests.swift
-//
-// The MIT License (MIT)
-//
-// Copyright (c) 2015 Zewo
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 @testable import TrieRouteMatcher
 import XCTest
 
 class TrieRouteMatcherTests: XCTestCase {
     let ok = Responder { request in
         return Response(status: .OK)
-    }
-
-    func testExample() {
-        XCTAssert(true)
     }
 
     func testTrie() {
@@ -303,9 +275,17 @@ class TrieRouteMatcherTests: XCTestCase {
 struct TestRoute: RouteType {
     let path: String
     let actions: [HTTP.Method: Action]
-    
+
     init(path: String, actions: [HTTP.Method: Action] = [:]) {
         self.path = path
         self.actions = actions
+    }
+}
+
+extension TrieRouteMatcherTests {
+    static var allTests: [(String, TrieRouteMatcherTests -> () throws -> Void)] {
+        return [
+           ("testTrieRouteMatcherMatchesRoutes", testTrieRouteMatcherMatchesRoutes),
+        ]
     }
 }
